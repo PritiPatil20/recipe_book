8dd9442f092180b7cf69003011b9f9e94d91af1e
Priti Patil <pritipatil@Pritis-MacBook-Pro.local>
2019-10-31 14:39:04 -0400

    testing action mailer on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "recipe_book_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   host = "http://recipebooklaunch.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
