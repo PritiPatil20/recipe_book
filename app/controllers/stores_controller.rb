dd97edf3d48b919e898f23af345bd2ca93a1384b
Priti Patil <pritipatil@Pritis-MBP.fios-router.home>
2019-11-02 20:50:30 -0400

    added env variable

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,7 +2,7 @@ class StoresController < ApplicationController
   def index
     ip_address = request.remote_ip
     search = Geocoder.search(ip_address)
-    client = GooglePlaces::Client.new("AIzaSyDVoRwOYkVV_9S6WNDIBW7rAr80niME_hA")
+    client = GooglePlaces::Client.new(ENV['API_KEY'])
     @stores = client.spots(42.4851,-71.4328, :types => 'grocery_or_supermarket', :radius => 5000)
 
     render :index
