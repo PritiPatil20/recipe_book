4dd43aa89fa6144453beb1cdc7e3d793f37c3867
Priti Patil <pritipatil@Pritis-MBP.fios-router.home>
2019-11-03 21:23:24 -0500

    made changes in stores controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,7 +3,7 @@ class StoresController < ApplicationController
     ip_address = request.remote_ip
     search = Geocoder.search(ip_address)
     client = GooglePlaces::Client.new(ENV['API_KEY'])
-    @stores = client.spots((result.first.latitude,result.first.longitude, :types => 'grocery_or_supermarket', :radius => 5000)
+    @stores = client.spots(result.first.latitude,result.first.longitude, :types => 'grocery_or_supermarket', :radius => 5000)
 
     render :index
   end
