a88906f8ed50fca6f15e253d9506868dd0a9bb3d
Priti Patil <pritipatil@Pritis-MacBook-Pro.local>
2019-11-08 15:34:06 -0500

    made change in ip address variable value

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,6 @@
 class StoresController < ApplicationController
   def index
-    # ip_address = request.remote_ip
-    ip_address = "71.184.147.233"
+    ip_address = request.remote_ip
     result = Geocoder.search(ip_address)
     client = GooglePlaces::Client.new(ENV['API_KEY'])
     @stores = client.spots(result.first.latitude,result.first.longitude, :types => 'grocery_or_supermarket', :radius => 5000)
